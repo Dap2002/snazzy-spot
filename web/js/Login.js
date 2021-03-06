683d3685d9b50b763b28731141e94fe3a84ab4a6
joely <joely_w@outlook.com>
2021-03-06 17:59:58 +0000

    Fixed overwrite again

--- web/js/Login.js
+++ web/js/Login.js
@@ -1,15 +1,17 @@
 class Login{
     login_user(){
         this.user_details = {
-            "email":$("#email").val(),
-            "password":$("#password").val()
+            "email": $("#email").val(),
+            "password": $("#password").val()
         };
         fetch('/api/login', {
             method: "POST",
             body: JSON.stringify(this.user_details),
             headers: {"Content-type": "application/json; charset=UTF-8"}
-        }).then(response => response.json()) .then(json => {
-            console.log(json);
+        }).then(response => response.json()).then(json => {
+            if (json.success) {
+                window.location.href = 'index.html'
+            }
         });
     }
 }
