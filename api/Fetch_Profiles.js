fea6e5898f323fb44fdf6fbe07a69183b8593f93
joely <joely_w@outlook.com>
2021-03-07 08:42:38 +0000

    Fixed inner join

--- api/Fetch_Profiles.js
+++ api/Fetch_Profiles.js
@@ -6,7 +6,7 @@ class Fetch_Profiles extends Database{
         this.user_id = user_id;
     }
     fetch_profiles(callback){
-        let sql = "SELECT * FROM users, views WHERE NOT users.id = "+this.user_id+" AND NOT(views.user1 = "+this.user_id+" AND views.user2 = users.id) ORDER BY users.id ";
+        let sql = "SELECT * FROM users U WHERE id NOT IN(SELECT user2 FROM views WHERE user1=?) AND U.id!=? ORDER BY U.id";
         Database.conn.query(sql, function(err, result){
             if (err) throw err;
             return callback(result);
